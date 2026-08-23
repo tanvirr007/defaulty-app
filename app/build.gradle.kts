4f06cbcce6c3b5a2e38a60ef9ed5ffb6400ded14
tanvirr007 <43490582+tanvirr007@users.noreply.github.com>
2026-08-23 21:03:48 +0600

    chore: bump version to 2.1.1
    - Update versionName to 2.1.1.
    - Bump versionCode to 2.
    
    ----------------------------------------
    
    TEST:
    - Verify printVersionName Gradle task output.
    
    ----------------------------------------
    
    Change-Id: I2e15ba020856410187c04c9fdccb5a5300002110
    Signed-off-by: tanvirr007 <43490582+tanvirr007@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "app.default.tanvir.info"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
